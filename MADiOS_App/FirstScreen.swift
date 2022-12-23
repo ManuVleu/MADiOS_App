8cacdbfd184ccc29e8503498d82ffdd529fc4a67
ManuVleu <manu.vleurick@student.hogent.be>
2022-12-23 15:30:23 +0100

    tutorial Screens en buttons gdn

---
+++ MADiOS_App/FirstScreen.swift
@@ -0,0 +1,49 @@
+//
+//  FirstScreen.swift
+//  MADiOS_App
+//
+//  Created by Manu Vleurick on 23/12/2022.
+//
+
+import UIKit
+
+class FirstScreen: UIViewController {
+    
+    let nextButton = UIButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupButton()
+        title = "First Screen"
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    func setupButton() {
+        view.addSubview(nextButton)
+        
+        nextButton.configuration = .filled()
+        nextButton.configuration?.baseBackgroundColor = .systemPurple
+        nextButton.configuration?.title = "Next screen"
+        
+        nextButton.addTarget(self, action: #selector(goToNextScreen), for: .touchUpInside)
+        
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            nextButton.widthAnchor.constraint(equalToConstant: 200),
+            nextButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+    }
+    
+    @objc func goToNextScreen() {
+        let nextScreen = SecondScreen()
+        nextScreen.title = "Second Screen"
+        navigationController?.pushViewController(nextScreen, animated: true)
+    }
+}
+
---
+++ MADiOS_App/SecondScreen.swift
@@ -0,0 +1,30 @@
+//
+//  SecondScreen.swift
+//  MADiOS_App
+//
+//  Created by Manu Vleurick on 23/12/2022.
+//
+
+import UIKit
+
+class SecondScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
